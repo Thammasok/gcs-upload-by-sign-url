f93c72f8a4de53acae53f5ba43d8be509d0bc0b0
Thammasok <jaranchai.nt@hotmail.com>
2025-03-10 10:07:18 +0700

    [Edited] format code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,33 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 export default function Home() {
-  const [signedUrl, setSignedUrl] = useState('');
-  const [publicUrl, setPublicUrl] = useState('');
+  const [signedUrl, setSignedUrl] = useState('')
+  const [publicUrl, setPublicUrl] = useState('')
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const file = formData.get("file") as File;
+    e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const file = formData.get('file') as File
     if (!file.size) {
-      alert("No file selected");
-      return;
+      alert('No file selected')
+      return
     }
 
-    const contentType = file.type;
-    const res = await fetch(`/api/signed-url?contentType=${contentType}`);
-    const { signedUrl, headers, publicUrl } = await res.json();
+    const contentType = file.type
+    const res = await fetch(`/api/signed-url?contentType=${contentType}`)
+    const { signedUrl, headers, publicUrl } = await res.json()
 
     const options = {
-      method: "PUT",
+      method: 'PUT',
       headers: headers,
       body: file,
-    };
+    }
 
-    await fetch(signedUrl, options);
+    await fetch(signedUrl, options)
 
-    setSignedUrl(signedUrl);
-    setPublicUrl(publicUrl);
-  };
+    setSignedUrl(signedUrl)
+    setPublicUrl(publicUrl)
+  }
 
   return (
     <div className='flex flex-col items-center justify-center min-h-screen py-2 w-full'>
@@ -42,13 +42,15 @@ export default function Home() {
       </form>
       {signedUrl && (
         <div className='flex flex-col justify-center w-full p-10'>
-          <div className="bg-gray-900">Signed URL:</div>
-          <p className="break-words">{signedUrl}</p>
-          <div className="bg-gray-900">Public URL:</div>
-          <a href={publicUrl} target="_blank">{publicUrl}</a>
+          <div className='bg-gray-900'>Signed URL:</div>
+          <p className='break-words'>{signedUrl}</p>
+          <div className='bg-gray-900'>Public URL:</div>
+          <a href={publicUrl} target='_blank'>
+            {publicUrl}
+          </a>
         </div>
       )}
     </div>
-  );
+  )
 }
 // http://localhost:3000/api/signed-url?contentType=video/mp4
